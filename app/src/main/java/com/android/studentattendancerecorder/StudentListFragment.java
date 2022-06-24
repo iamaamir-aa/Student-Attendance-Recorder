0f2941b2f3afdd53b574b1ee21cf9fd80c93f597
Aamir <mohdaamir7674@gmail.com>
2022-06-24 17:24:11 +0530

    Implement Safe Args From Class List to Student List

--- app/src/main/java/com/android/studentattendancerecorder/StudentListFragment.java
+++ app/src/main/java/com/android/studentattendancerecorder/StudentListFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class StudentListFragment extends Fragment {
@@ -21,6 +22,10 @@ public class StudentListFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        TextView countTV=view.findViewById(R.id.tv);
+        int count=StudentListFragmentArgs.fromBundle(getArguments()).getPosition();
+        countTV.setText(String.valueOf(count));
 
     }
 
